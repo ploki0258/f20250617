44508c04e6f41f7fc6365d90c0980ea703ed17be
Jack_Chiu <qqwer12313@gmail.com>
2025-08-16 23:40:48 +0800

    JavaScript基礎介紹_20250816

--- dist/js/20250814-1-js-basic.js
+++ dist/js/20250814-1-js-basic.js
@@ -1,3 +1,5 @@
+console.log("In the script");
+
 let num = 123;
 console.log(num);
 num = 100;
@@ -33,8 +35,9 @@ let num1 = 10;
 let num2 = "9";
 console.log(num1 - num2);
 let sub = num1 - "a";
-console.log(sub, typeof sub);
+console.log(`sub: ${sub}, type: ${typeof sub}`);
 
+// 物件 Object
 let student = {
     name: "David Lin",
     age: 18,
@@ -45,3 +48,13 @@ let student = {
 };
 console.log(student.name);
 student.SayHello();
+
+const students = {
+    name: "David Lin",
+    age: 18,
+    gender: "male",
+};
+students.name = "John Doe"; // 可以修改屬性
+// students = "A"; // 不能重新指派整個物件
+console.log(students.name);
+console.log(students);
--- dist/js/20250814-2-calculate.js
+++ dist/js/20250814-2-calculate.js
@@ -3,13 +3,14 @@ let dom = {
     num2: document.querySelector("#num2"),
     operator: document.querySelector("#operator"),
     btnCalculate: document.querySelector("#btn-calculate"),
-    result: document.querySelector("#result"),
+    result: document.querySelector("#result .result"),
+    tip: document.querySelector("#result .tip"),
 };
 
 // 檢查 dom 是否存在
 console.log(dom);
 
-dom.btnCalculate.addEventListener("click", function () {
+dom.btnCalculate.addEventListener("click", () => {
     // 測試事件是否正常觸發
     console.log("click btn");
 
@@ -25,22 +26,43 @@ dom.btnCalculate.addEventListener("click", function () {
     }
 
     // 從 DOM 中取得的數值，一定都是字串
-    num1 = parseInt(num1, 10);
+    // num1 = parseInt(num1, 10);   // 將字串轉為整數(傳統作法)
+    num1 = +num1; // 將字串轉為數字
     num2 = +num2;
 
+    // 加法
     if (operator === "add") {
         console.log("run add.");
         let result = num1 + num2;
         dom.result.textContent = result;
+        dom.tip.textContent = "加法結果：";
     }
 
+    // 減法
     if (operator === "sub") {
         console.log("run sub.");
         let result = num1 - num2;
         dom.result.textContent = result;
+        dom.tip.textContent = "減法結果：";
     }
 
     // 乘法
+    if (operator === "mul") {
+        console.log("run mul.");
+        let result = num1 * num2;
+        dom.result.textContent = result;
+        dom.tip.textContent = "乘法結果：";
+    }
 
     // 除法
+    if (operator === "div") {
+        console.log("run div.");
+        if (num2 === 0) {
+            alert("除數不能為零");
+            return false;
+        }
+        let result = num1 / num2;
+        dom.result.textContent = result;
+        dom.tip.textContent = "除法結果：";
+    }
 });
